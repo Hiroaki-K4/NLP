2d2887e973573c2f76b7497fee2121755446d7a6
Hiroaki-K4 <49no19@gmail.com>
2021-08-06 09:27:02 +0900

    make SVD

--- layers.py
+++ layers.py
@@ -16,3 +16,41 @@ class Matmul:
 		dw = np.dot(self.x.T, dout)
 		self.grads[0][...] = dw
 		return (dx)
+
+
+class Sigmoid:
+	def __init__(self):
+		self.params, self.grads = [], []
+		self.out = None
+
+	def forward(self, x):
+		out = 1 / (1 + np.exp(-x))
+		self.out = out
+		return out
+
+	def backward(self, dout):
+		dx = dout * (1.0 - self.out) * self.out
+		return dx
+
+
+class Affine:
+	def __init__(self, w, b):
+		self.params = [w, b]
+		self.grads = [np.zeros_like(w), np.zeros_like(b)]
+		self.x = None
+
+	def forward(self, x):
+		w, b = self.params
+		out = np.dot(x, w) + b
+		self.x = x
+		return out
+
+	def backward(self, dout):
+		w, b = self.params
+		dx = np.dot(dout, w.T)
+		dy = np.dot(self.x.T, dout)
+		db = np.sum(dout, axis=0)
+
+		self.grads[0][...] = dw
+		self.grads[1][...] = db
+		return dx
